d6e497b8ed6f605b1edd5d50609f4f794ebb29e2
Sufyanmd <mdsufyan.b.2020@gmail.com>
2025-07-10 16:28:03 +0530

    File setup and navbar code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from './assets/comonents/Logo'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-black'>
+      <div className='container text-gray-200 py-3'> 
+        <Logo/>
+      </div>
+    </div>
+  )
+}
+
+export default App
