2983273dd2664fc0b7a96c9c358a48ff1b65708a
Sufyanmd <mdsufyan.b.2020@gmail.com>
2025-07-12 17:39:21 +0530

    Resolved loding problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Logo from './assets/components/Logo'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Logo from './assets/Logo';
+import Users from './assets/Routes/Users';
+import Userinfo from './assets/Routes/Userinfo';
 
 const App = () => {
   return (
     <div className='min-h-screen bg-black'>
-      <div className='container text-gray-200 py-3'> 
-        <Logo/>
+      <div className='container text-gray-200 py-3'>
+        <Logo />
+        <Routes>
+          <Route path='/' element={<Users />} />
+          <Route path='/:username' element={<Userinfo />} />
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
